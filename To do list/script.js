aa70dcf859acb28b6f8852ee079f14be6d434b93
toyin2022 <shevioni@yahoo.com>
2025-04-22 19:52:01 +0100

    Refactor form submission handling and improve HTML structure

--- To do list/script.js
+++ To do list/script.js
@@ -1,30 +1,30 @@
-document.querySelector('#push').onclick = function(){
-    if(document.querySelector('#newtask input').value.length == 0){
-        alert("Please enter a task")
-    }
-    else{
-        document.querySelector("#tasks").innerHTML += `
+document.querySelector("#newtask").onsubmit = function (event) {
+  event.preventDefault(); // Prevent form from reloading the page
+  if (document.querySelector("#newtask input").value.length == 0) {
+    alert("Please enter a task");
+  } else {
+    document.querySelector("#tasks").innerHTML += `
         <div class="task">
-            <span id= "taskname">
+            <span id="taskname">
             ${document.querySelector("#newtask input").value}
             </span>
-             <button class="delete">
-                  <img src='trash-can-regular.svg' alt=""  height='20' width='20';"/>
+            <button class="delete">
+                <img src='trash-can-regular.svg' alt="" height='20' width='20';"/>
             </button>
         </div>
         `;
-        var current_tasks = document.querySelectorAll(".delete");
-        for(var i=0; i<current_tasks.length; i++){
-            current_tasks[i].onclick = function(){
-                this.parentNode.remove();
-            }
-        }
-        var tasks = document.querySelectorAll(".task");
-        for (var i =0; i<tasks.length; i++){
-            tasks[i].onclick = function(){
-                this.classList.toggle('completed');
-            }
-        }
-        document.querySelector("#newtask input").value = "";
+    var current_tasks = document.querySelectorAll(".delete");
+    for (var i = 0; i < current_tasks.length; i++) {
+      current_tasks[i].onclick = function () {
+        this.parentNode.remove();
+      };
     }
-}
+    var tasks = document.querySelectorAll(".task");
+    for (var i = 0; i < tasks.length; i++) {
+      tasks[i].onclick = function () {
+        this.classList.toggle("completed");
+      };
+    }
+    document.querySelector("#newtask input").value = "";
+  }
+};
